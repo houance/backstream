9e08848f9ca1f5da6ef77fdf3d2fcb711c1444ac
houance <837507557@qq.com>
2026-01-22 13:23:27 +0800

    更改 db 文件存储位置

--- server/src/service/db/client.ts
+++ server/src/service/db/client.ts
@@ -3,7 +3,7 @@ import Database from 'better-sqlite3';
 import * as schema from './schema.js';
 
 // Creates the sqlite file if it doesn't exist
-const sqlite = new Database('backup_app.db');
+const sqlite = new Database('./db/backup_app.db');
 
 // Export the singleton instance
 export const db = drizzle(sqlite, { schema });
