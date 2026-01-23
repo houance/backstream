7ed0a72cce06692e8e73d392dff363f071ac1a26
houance <837507557@qq.com>
2026-01-23 21:26:52 +0800

    修改 enum 为 const, build 的时候排除 test

--- server/src/service/restic/types.ts
+++ server/src/service/restic/types.ts
@@ -1,24 +1,28 @@
 import type {Result} from "execa";
 
-export enum ExitCode {
-    Success = 0,
-    Failure = 1,
-    GoRuntimeError = 2,
-    BackupReadError = 3,
-    RepositoryDoesNotExist = 10,
-    FailedToLockRepository = 11,
-    WrongPassword = 12,
-    Interrupted = 130,
-    UNKNOWN = -1, // not recognized exit code
-}
+export const ExitCode = {
+    Success: 0,
+    Failure: 1,
+    GoRuntimeError: 2,
+    BackupReadError: 3,
+    RepositoryDoesNotExist: 10,
+    FailedToLockRepository: 11,
+    WrongPassword: 12,
+    Interrupted: 130,
+    UNKNOWN: -1,
+} as const;
 
-export enum RepoType {
-    LOCAL = "LOCAL",
-    SFTP = "SFTP",
-    BACKBLAZE_B2 = "BACKBLAZE_B2",
-    ALIYUN_OSS = "ALIYUN_OSS",
-    S3 = "S3",
-}
+export type ExitCode = typeof ExitCode[keyof typeof ExitCode];
+
+export const RepoType = {
+    LOCAL: "LOCAL",
+    SFTP: "SFTP",
+    BACKBLAZE_B2: "BACKBLAZE_B2",
+    ALIYUN_OSS: "ALIYUN_OSS",
+    S3: "S3",
+} as const;
+
+export type RepoType = typeof RepoType[keyof typeof RepoType];
 
 export interface RepoConfig {
     version: number;
